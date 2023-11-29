8d88a93bb32b4e63ee601b796de424ac68d9038a
pedrodecf <contato.pedrodecf@gmail.com>
2023-11-29 12:52:37 -0300

    chore: app route configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+// Dependências do React
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+// Estilos globais
 import GlobalStyle from "./styles/global"
+
+// Rotas da aplicação
+import { Routes } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <Routes />
   </React.StrictMode>
 )
---
+++ src/routes/auth.routes.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+
+import { SignIn } from "../pages/SignIn"
+
+export function AuthRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+    </Routes>
+  )
+}
