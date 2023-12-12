a285c306aa18d1c9d7798026d772431f0a23c55e
pedrodecf <contato.pedrodecf@gmail.com>
2023-12-12 20:05:06 -0300

    feat: add onkeydown to searchbar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,10 +1,17 @@
 import { Container } from "./style"
 
 export function SearchBar({ icon, onClick, ...rest }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onClick()
+    }
+  }
   return (
     <Container>
-      <button type="button" onClick={onClick}>{icon}</button>
-      <input {...rest} />
+      <button type="button" onClick={onClick}>
+        {icon}
+      </button>
+      <input {...rest} onKeyDown={handleKeyDown} />
     </Container>
   )
 }
